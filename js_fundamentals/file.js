52a610d48cb165d4e9d9226e668a99a5fd75eaaa
Vuyo Sopangisa <vssopangisa@gmail.com>
2023-03-29 16:08:50 +0200

    new updates

---
+++ js_fundamentals/file.js
@@ -0,0 +1,48 @@
+//var a=1;
+//var a=10;
+
+// let a=1
+// let a=10
+
+// let a=1;
+// let b=2;
+// let c=a+b;
+//let d=20;
+//let c=10;
+//let a=70;
+//let b=20;
+
+//let a=5;
+//let b=10;
+
+//let a=10
+
+
+
+//if (a > 10) {
+  //  console.log("Greater than a decade");
+//}else if(a ==10){
+   // console.log("If is a decade")
+//}else if (a < 10 && a >=10){
+  //  console.log("It is lesser than a decade")
+//}else{
+  //  console.log("We cannot have negative years!")
+//}
+
+let a=10
+let b=5
+let c=15
+
+if (a>b && a>c) {
+    console.log(a)
+}else if (b>a && b>c) {
+    console.log(b)
+} else if(c>a && c>b) {
+    console.log(c)
+}
+
+
+
+
+
+
