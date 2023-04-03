b575044fb726cf8dd32ed1dcbe68918caa519843
Vuyo Sopangisa <vssopangisa@gmail.com>
2023-04-03 16:54:04 +0200

    new updates

--- js_fundamentals/array.js
+++ js_fundamentals/array.js
@@ -7,7 +7,7 @@
 //myarray.unshift(5);
 //console.log(myarray)
  
-let array = [20,311,12,11,13,15,9,4,0,10]
+/*let array = [20,311,12,11,13,15,9,4,0,10]
 let even  = new Array();
 let odd  = new Array();
 for (let v=0; v < array.length; v++)
@@ -18,7 +18,74 @@ for (let v=0; v < array.length; v++)
   }
 
 console.log(even)
-console.log(odd)
+console.log(odd) */
+
+/*let letters = ["b","c","Z","p","y","Q","l","V"]
+letters.sort()
+console.log(letters)*/
+
+/*function maxAndMin(numbers) {
+    let obj = {}
+    numbers.sort((a,b) => a-b)
+    obj["minNumber"] = numbers[0]
+    obj["maxNumber"] = numbers[numbers.length-1]
+
+    return obj
+
+}
+maxAndMin([6,7,3,2,1])
+let firstApp = maxAndMin([6,7,3,2,1])
+
+console.log(firstApp)*/
+
+
+/*let numbers = [-3,-100,10,4,6,7,20,1]
+let 
+numbers.sort((a,b)=> a-b)
+
+console.log(numbers)*/
+
+/*let myname = "Vuyo"
+let reversedname ="oyuV"
+
+console.log(reversedname)*/
+
+/*let myname = "Vuyo"
+let reversedname ="oyuV"*/
+
+/*function strReversed (text) {
+    let reversed ="";
+    for (let i=0; i< 10; i++) {
+     reversed = text[i] + reversed;
+    }
+    console.log(reversed) ;
+}
+strReversed()
+strReversed("JavaScript is super easy")*/
+
+/*function calcSalary(hourlyRate,hours,days,pci=1) {
+    
+    return hourlyRate*hours*days*pci
+
+}
+console.log(calcSalary (32,7,20,1))*/
+
+/*function emailGen(fname="Vuyo",lname="Sopa") {
+
+    let emailGen = fname+"."+lname+"@gsb.co.za"
+    return emailGen. toUpperCase()
+}
+console.log(emailGen('Vuyo','Sopangisa'))*/
+
+let num1 = [1,3,5,7]
+let num2 = [0,2,4,6]
+let num3 = [...num1,...num2]
+let num4 = num1.concat(num2)
+
+console.log("This is num3",num3)
+console.log("This is num4",num4)
+
+
 
 
 
