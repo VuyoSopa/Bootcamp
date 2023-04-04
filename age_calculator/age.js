a56c0cc8f1c16b0ee5b22147daadaeea9f7f42e8
Vuyo Sopangisa <vssopangisa@gmail.com>
2023-04-04 16:55:59 +0200

    age calculator

---
+++ age_calculator/age.js
@@ -0,0 +1,16 @@
+function calcAge(years){
+    let age = (2023-years);
+    if(age < 0) {
+        return("error,age cannot be negative")
+    }
+    else if (age >= 0)
+    {
+        return age
+    }
+    else if ('bornYear'){
+        return "Please enter your born Year"
+    }
+       
+}
+console.log(calcAge())
+    
---
+++ age_calculator/return.js
@@ -0,0 +1,14 @@
+/*function addTen(a) {
+return a+10
+}
+console.log(addTen(7))*/
+
+/*function multiplyByFive(m) {
+    return m*5
+}
+console.log(multiplyByFive(3))*/
+
+function sumTwovalues(c,d) {
+    return c+d
+}
+console.log(sumTwovalues(9,1))
