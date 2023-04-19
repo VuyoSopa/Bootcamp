39455302d283509ed5e6061355cf01ff223cc41b
Vuyo Sopangisa <vssopangisa@gmail.com>
2023-04-19 16:32:59 +0200

    updates

--- age_calculator/age.js
+++ age_calculator/age.js
@@ -1,3 +1,7 @@
+ let yearAns =[]
+ localStorage.setItem("calculator",JSON.stringify(yearAns))
+  //age=JSON.parse(localStorage.getItem("calculator"))
+
 function submit() {
   let year = document.getElementById("submit").value;
   if (year < 0) {
@@ -6,15 +10,23 @@ function submit() {
     alert("Please enter your born Year");
   } else {
     let age = 2023 - year;
-
+  
     if (age <= 0) {
       alert("error,age cannot be negative or zero");
     } else if (age >= 0) {
       alert(age);
     }
+    //number = document.getElementById('calculator').value
   }
+}
+function history(){
+  let age = document.getElementById('calculator').value
+  age.unshift(age)
+  (localStorage.getItem("calculator"))
+   yearAns =JSON.parse(localStorage.getItem("calculator"));
 
+  }//yearAns.push(history)
   // console.log(age);
   // console.log(years);
-  return false;
-}
+  //return false;
+
--- image_slider/script.js
+++ image_slider/script.js
@@ -13,10 +13,26 @@ prev.addEventListener('click', () => {
 // let prev = document.querySelector('prev')
 // let next = document.querySelector('next')
 
+let images;
 
+if(localStorage.getItem("slider")){
+images=JSON.parse(localStorage.getItem("slider"));
+}
+else{
+images = [
+    "joker.jpg",
+    "wallpapers.jpg",
+    "anime.jpg",
+    "met.webp",
+    "harley.webp",
+    "cartoon.jpg"
+];
+} //localStorage.removeItem()
+/*if(localStorage.getItem("slider")  images ) {
+  alert("Picture already exist")
+}*/
 
-
-let images= [
+/*let photos= [
     "joker.jpg",
     "wallpapers.jpg",
     "anime.jpg",
@@ -25,18 +41,47 @@ let images= [
     "cartoon.jpg"
 ];
 
+localStorage.setItem('slider',JSON.stringify(photos))
+let slider1 = localStorage.getItem("slider")
+let slider = JSON.parse()
+console.log(slider);
+
+/*alert(photos2.length);*/
+
 function addimage() {
-  Img = document.getElementById('images').value
- images.push(Img)
- //console.log(Img);
+  let img = document.getElementById('images').value;
+    //images.push(Img)
+    if(!img){
+      return alert("Please insert image")
+  }
+    for (let i = 0; i < images.length; i++)
+      {
+      
+      if(img == images[i]) {
+      return alert("Picture already exist")
+    }
+      
+    
+      }
+      images.push(img)
+  //console.log(Img);
+
+  localStorage.setItem("slider",JSON.stringify(images));
+  document.getElementById('images').value='';
+
 }
 
-let index = 0; 
+/*function removeDuplicates {
+if(localStorage.getItem("slider") == "images" ) {
+  alert("Picture already exist")
+ //document.getElementById('Images').value='';*/
 
-img = document.getElementById('image')
+
+
+ let index = 0;
+
 function next() {
-   //img = document.getElementById('image')
-    
+   img = document.getElementById('image')
    // console.log(show);
      index ++;
     // image.src = images[index]
@@ -50,7 +95,7 @@ function next() {
 //next()
 
 function prev() {
-  //  img = document.getElementById('image')
+    let img = document.getElementById('image')
    // console.log(show);
      index --;
     // image.src = images[index]
@@ -60,6 +105,48 @@ function prev() {
      img.src = images [index];
   
 }
+
+function removeImage(){
+
+  //images= document.getElementById('remove').value
+  images.splice(index,1)
+  localStorage.setItem("slider",JSON.stringify(images));
+  return false
+}
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let photos= [
+  "joker.jpg",
+    "wallpapers.jpg",
+    "anime.jpg",
+    "met.webp",
+    "harley.webp",
+    "cartoon.jpg"
+];
+
+localStorage.setItem("slider",JSON.stringify(photos))*/
 
 
 
